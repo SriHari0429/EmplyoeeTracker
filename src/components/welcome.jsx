dc26c96b2d04f3e2b9697c0f91be9b3d6f30e270
Srihari <sriharinaidu951@gmail.com>
2025-03-13 15:53:28 +0530

    renamed

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Welcome.css"; // Import styles
+import "./welcome.css"; // Import styles
 
 const Welcome = () => {
   const navigate = useNavigate();
